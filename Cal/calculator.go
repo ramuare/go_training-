829c48c6cda8d4ad946000d5fbde0a549d36b367
ramuare <kodandaramaprasadare@gmail.com>
2022-05-02 16:58:08 -0500

    small changes

---
+++ Cal/calculator.go
@@ -0,0 +1,19 @@
+package Cal
+
+import "fmt"
+
+func Add(x int, y int) {
+	fmt.Println(x + y)
+}
+
+func Sub(x int, y int) {
+	fmt.Println(x - y)
+}
+
+func Mult(x int, y int) {
+	fmt.Println(x - y)
+}
+
+func Div(x int, y int) {
+	fmt.Println(x - y)
+}
