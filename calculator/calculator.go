12d6b9a012b3f52e7b1fbc06fab2501be5e9b77d
ramuare <kodandaramaprasadare@gmail.com>
2022-05-02 16:49:31 -0500

    added calc program

---
+++ calculator/calculator.go
@@ -0,0 +1,19 @@
+package Calculator
+
+import "fmt"
+
+func Add(x int, y int) {
+	fmt.Println(x + y)
+}
+
+func Sub(x int, y int) {
+	fmt.Println(x - y)
+}
+
+func Mult(x int, y int) {
+	fmt.Println(x - y)
+}
+
+func Div(x int, y int) {
+	fmt.Println(x - y)
+}
